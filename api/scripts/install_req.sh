d06b2e72631f495eaacdd75e70534a53160ad7f2
TunSiang <78256625+TunSiang@users.noreply.github.com>
2021-12-15 14:22:34 +0800

    Update install_req.sh

--- api/scripts/install_req.sh
+++ api/scripts/install_req.sh
@@ -8,7 +8,8 @@ sudo apt install net-tools
 
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt-get update && sudo apt-get install vagrant
+sudo apt-get update
+sudo apt-get install vagrant
 
 sudo pip3 install python-vagrant
 
